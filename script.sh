9ee21a67f65742a025cf81b718ce1a2b7207d009
Yu-Cheng <tigernaxo.co.cc@gmail.com>
2020-10-30 08:37:10 +0000

    finish

--- script.sh
+++ script.sh
@@ -33,7 +33,36 @@ git commit --allow-empty -m "Initializing gh-pages branch"
 git push origin gh-pages
 git checkout main
 
-if [ "`git status -s`" ]
+#if [ "`git status -s`" ]
+#then
+#    echo "The working directory is dirty. Please commit any pending changes."
+#    exit 1;
+#fi
+#
+#echo "Deleting old publication"
+#rm -rf public
+#mkdir public
+#git worktree prune
+#rm -rf .git/worktrees/public/
+#
+#echo "Checking out gh-pages branch into public"
+#git worktree add -B gh-pages public origin/gh-pages
+#
+#echo "Removing existing files"
+#rm -rf public/*
+#
+#echo "Generating site"
+#hugo
+#
+#echo "Updating gh-pages branch"
+#cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+#
+##echo "Pushing to github"
+#git push --all
+
+FILE="run_publish.sh"
+cat > $FILE <<- EOF
+if [ "\`git status -s\`" ]
 then
     echo "The working directory is dirty. Please commit any pending changes."
     exit 1;
@@ -55,7 +84,10 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (run_publish.sh)"
 
 #echo "Pushing to github"
 git push --all
+EOF
+chmod a+x ${FILE}
+./${FILE}
